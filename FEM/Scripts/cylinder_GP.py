67d38c3eb5741edb58188b1d4ce003e5b1823ed4
metadev <haralddebondt@gmail.com>
2017-09-25 15:13:11 +0200

    fix error, add gp

--- FEM/Scripts/cylinder_GP.py
+++ FEM/Scripts/cylinder_GP.py
@@ -1,16 +1,14 @@
-import keras
-from keras.models import Sequential
-from keras.layers import Dense, Activation
-from keras.layers.core import Dropout
-from keras.wrappers.scikit_learn import KerasRegressor
+import matplotlib.pyplot as plt
 import numpy as np
 import sklearn.model_selection as ms
-import matplotlib.pyplot as plt
-from sklearn.preprocessing import StandardScaler
-from sklearn.preprocessing import normalize
-import vector_calc as vc
 import xlwings as xw
-
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import ConstantKernel as C
+from sklearn.gaussian_process.kernels import RBF, WhiteKernel
+from sklearn.preprocessing import StandardScaler, normalize
+from sklearn.cross_validation import cross_val_score, KFold
+from sklearn import linear_model
+import vector_calc as vc
 
 # read input
 data_path = "C:\\Users\\Administrator\\Google Drive\\Windows\\Research\\Project\\FEM\\Results\\data"
@@ -18,7 +16,9 @@ result_path = r'C:\Users\Administrator\Google Drive\Windows\Research\Project\Doc
 data_type = 2
 n_sensor_axis = 1
 axis_training = {1: [0], 2: [0, 1], 3: [0, 1, 2]}
+# name_afo_project = "_cylinder"
 name_afo_project = "_simple_afo_big"
+
 if data_type == 0:
     x_file = "\\all_strain_list_simple" + name_afo_project
 elif data_type == 1:
@@ -47,29 +47,6 @@ def preprocessing_data(X, Y, n_sensors):
     X=X+noise
     Y = scalerY.fit_transform(Y)
     return X, Y
-
-
-def build_model(X):
-    n_input = len(X[0])
-    # create model
-    model = Sequential()
-    model.add(Dense(n_input, input_dim=n_input,
-                    kernel_initializer='normal', activation='relu'))
-    # adding more than two layer actually lower accuracy, while it should be overfitting, this could be due to decreased compatibility with other hyperparameters
-    model.add(Dense(32))
-    model.add(Activation('relu'))
-    model.add(Dropout(0.5))
-
-    #2d displacement
-    model.add(Dense(2, kernel_initializer='normal'))
-    adam = keras.optimizers.Adam(
-        lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
-    keras.callbacks.EarlyStopping(
-        monitor='val_loss', min_delta=0.02, patience=10, verbose=0, mode='auto')
-    # Compile model
-    model.compile(loss='mse', optimizer=adam, metrics=['mse', 'accuracy'])
-    return model
-
 def evaluate_by_example(X_pre, Y,model):
     # #pick strain values from X, with a label from -40 to 20
     # to_pred_points = np.linspace(-4,2,7)
@@ -87,55 +64,52 @@ def evaluate_by_example(X_pre, Y,model):
     #return list of avtual deformation angle in degrees and it's corresponding error
     return list(zip([calc_angle_degree((t_y),4) for t_y in test_Y],[e for e in degree_error]))
 
-    # the length and dsiplacment should both have the same unit
-    # in my test setup the displacement is in dm and length in mm
-def calc_angle_degree(displacement,length):
-    #the first ordinal is x, the second y
-    return np.rad2deg(np.tanh(displacement/length))
 
-n_trials = 5
-xl_arr = []
+# Instanciate a Gaussian Process model
+kernel = 1.0 * RBF() \
+    + WhiteKernel()
+
+gp = GaussianProcessRegressor(kernel=kernel,alpha=0, n_restarts_optimizer=9)
+# # Initialize model.
+# logreg = linear_model.LinearRegression()
+# X_pre, Y_pre = preprocessing_data(X, Y,10)
+# # Use cross_val_score to automatically split, fit, and score.
+# scores = ms.cross_val_score(gp, X_pre, Y_pre, cv=10,scoring="r2")
+# print(scores)
+n_trials = 1
+cv_results = []
 degree_test= []
-xl_degree_test= []
 for i, n_s in enumerate(range(10, 11)):
     # for evaluation of a model accuracy the sensor layout should be kept constant over trials 
     # X_pre, Y_pre = preprocessing_data(X, Y, n_s)
 
-    cv_score_acc_mean = []
-    cv_score_acc_std = []
+    mse=[]
     for n in range(n_trials):
         #pick a different layout for each trial for angle evaluation
         X_pre, Y_pre = preprocessing_data(X, Y, n_s)
         # to average out differences in results of the model, NN trains stochastically
-        model = build_model(X_pre)
-
+        model = gp
         # fix random seed for reproducibility
         seed = 7
         kfold = ms.KFold(n_splits=5, random_state=seed)
-        cvscores_mse = []
-        cvscores_val_acc = []
+        cv_score_mse = []
+
         for train, test in kfold.split(X_pre, Y_pre):
             # Fit the model
-            history = model.fit(X_pre[train], Y_pre[train], epochs=100,
-                                batch_size=10, verbose=0)
-            
+            model.fit(X_pre[train], Y_pre[train])
             # evaluate the model
-            scores = model.evaluate(X_pre[test], Y_pre[test], verbose=0)
-            #print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
-            cvscores_val_acc.append(scores[2] * 100)
-            cvscores_mse.append(scores[1] * 100)
+
+            y_mean, y_cov = gp.predict(X_pre[test], return_cov=True)
+            y_error = np.mean((y_mean-Y_pre[test])**2)
+            mse.append(y_error * 100)
             
-        # print("mse")
-        # print("%.2f%% (+/- %.2f%%)" % (np.mean(cvscores_mse), np.std(cvscores_mse)))
-        # print("accuracy")
-        degree_test.append(evaluate_by_example(X_pre, Y,model))
-        cv_score_acc_mean.append(np.mean(cvscores_val_acc))
-        cv_score_acc_std.append(np.std(cvscores_val_acc))
+        # degree_test.append(evaluate_by_example(X_pre, Y,model))
+        cv_score_mse.append([np.mean(mse),np.std(mse)])
+        
     #calc mean and std from degree test
-    xl_arr.append([n_s, np.mean(cv_score_acc_mean),
-                   np.mean(cv_score_acc_std)])
-print(np.mean(degree_test,axis=0).reshape(len(degree_test[0]),-1))
-print(np.std(degree_test,axis=0).reshape(len(degree_test[0]),-1)[:,2:4])
+    degree_test.append(evaluate_by_example(X_pre, Y,model))
+    cv_results.append([n_s, np.mean(np.array(cv_score_mse)[:,0]),
+    np.mean(np.array(cv_score_mse)[:,1])])
 
 #plot heatmap
 test_mean=np.mean(degree_test,axis=0).reshape(len(degree_test[0]),-1)
@@ -144,7 +118,6 @@ A =np.stack((
      test_mean[:,0],
      test_mean[:,1],
      test_mean[:,2]+test_mean[:,3]),axis=1)
-
 from scipy.interpolate import griddata
 # grid the data.
 # define grid.
@@ -159,30 +132,3 @@ plt.colorbar() # draw colorbar
 # plt.scatter(test_mean[:,0],test_mean[:,1],marker='o',c='b',s=5)
 plt.savefig("heatmap_degree_error.png")
 plt.show()
-
-# save results in excel
-# import xlwings as xw
-# wb = xw.Book(result_path)  # this will create a new workbook
-# import datetime
-# sht = wb.sheets.add(datetime.datetime.now().strftime(
-#     '%m-%d-%h-%M') + " axisdim-" + str(n_sensor_axis))
-# sht.range('A1').value = np.concatenate( 
-#     (np.mean(degree_test,axis=0).reshape(7,-1),
-#     np.std(degree_test,axis=0).reshape(7,-1)[:,2:4]),axis=1)
-
-# # summarize history for accuracy
-# plt.plot(history.history['acc'])
-# plt.plot(history.history['val_acc'])
-# plt.title('model accuracy')
-# plt.ylabel('accuracy')
-# plt.xlabel('epoch')
-# plt.legend(['train', 'test'], loc='upper left')
-# plt.show()
-# # summarize history for loss
-# plt.plot(history.history['loss'])
-# plt.plot(history.history['val_loss'])
-# plt.title('model loss')
-# plt.ylabel('loss')
-# plt.xlabel('epoch')
-# plt.legend(['train', 'test'], loc='upper left')
-# plt.show()
